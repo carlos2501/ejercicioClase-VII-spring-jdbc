f0d5b50a4a7076d67fbc595c68ba04429294220d
CPL <carlos@centropyme.com>
2025-06-03 05:37:46 +0200

    PASO 5 * Creamos un único servicio para las tres funcionalidades (requisitos), ya que son muy pocas y por legibilidad del código no merece la pena separarlas. * Creamos un controlador para el menu de inicio, otro para las oficinas y un tercero para visualizar los datos de los empleados. * Se pasa el código desde el main al controlador correspondiente.

--- src/main/java/org/cpl_cursos/ejercicioClase_VII_spring_jdbc/EjercicioClaseViiSpringJdbcApplication.java
+++ src/main/java/org/cpl_cursos/ejercicioClase_VII_spring_jdbc/EjercicioClaseViiSpringJdbcApplication.java
@@ -1,18 +1,11 @@
 package org.cpl_cursos.ejercicioClase_VII_spring_jdbc;
 
-import org.cpl_cursos.ejercicioClase_VII_spring_jdbc.DTOs.EmpleadoDTOLista;
-import org.cpl_cursos.ejercicioClase_VII_spring_jdbc.DTOs.VentasEmpleadoDTO;
 import org.cpl_cursos.ejercicioClase_VII_spring_jdbc.controladores.JardineriaCtrl;
-import org.cpl_cursos.ejercicioClase_VII_spring_jdbc.repositorios.EmpleadoRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
 
 @SpringBootApplication
 public class EjercicioClaseViiSpringJdbcApplication implements ApplicationRunner {
